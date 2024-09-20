23cc1f784d44940e44488ec094a083186d9deb69
SagaOfAGuy <bandoodle125@gmail.com>
2024-09-19 20:53:07 -0400

    Re-added next in options

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.options('*', (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
     res.sendStatus(204);
+	next();
 });
 
 
