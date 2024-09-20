a71e958c0013f3fdadf4e4f3b734bd3e0ac73d29
SagaOfAGuy <bandoodle125@gmail.com>
2024-09-19 20:47:34 -0400

    New CORS change

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const corsOptions = {
 };
 */ 
 
-
+/*
 // Apply CORS option to the express app
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -43,15 +43,16 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
 	next();
 });
+*/ 
 
-/*
+
 app.options('*', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
     res.sendStatus(204);
 });
-*/ 
+
 
 
 
